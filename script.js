835f36e60aac237880c49522e4c802a96c13cc1c
himegoodandipoop2 <himegoodandipoop2@gmail.com>
2025-08-29 07:30:45 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Invite & Support button actions
+document.getElementById("inviteBtn").onclick = () => {
+  window.open("https://discord.com/oauth2/authorize?client_id=1408602974736879628&permissions=8&integration_type=0&scope=bot", "_blank");
+};
+
+document.getElementById("supportBtn").onclick = () => {
+  window.open("https://discord.gg/zwJVvBHvtD", "_blank");
+};
